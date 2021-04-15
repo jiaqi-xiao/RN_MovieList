0eb86c7daf158146a568a99d63a33e8ec56a80ec
Jiaqi Xiao <xiaojiaqi03@kuaishou.com>
2021-04-15 14:58:39 +0800

    code cleanup

--- App.js
+++ App.js
@@ -7,7 +7,6 @@
  */
 
 import React, {Component} from 'react';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,109 +14,14 @@ import {
   View,
   Image,
   Dimensions,
-  useColorScheme,
 } from 'react-native';
 
 import {RecyclerListView, DataProvider, LayoutProvider} from 'recyclerlistview';
 import {DataCall} from './utils/DataCall';
-// import {ImageRenderer} from './components/ImageRenderer';
-// import {ViewSelector} from './components/ViewSelector';
-
-// const App: () => Node = () => {
-//   const [data, setData] = useState([]);
-
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   const RenderItem = item => {
-//     return (
-//       <View style={styles.itemRow}>
-//         <Image
-//           style={styles.logo}
-//           source={{
-//             uri: item.url,
-//           }}
-//         />
-//         <View style={{flex: 1}}>
-//           <Text
-//             style={[
-//               styles.title,
-//               {
-//                 color: isDarkMode ? Colors.white : Colors.black,
-//               },
-//             ]}>
-//             {item.title}
-//           </Text>
-//         </View>
-//       </View>
-//     );
-//   };
-
-//   useEffect(() => {
-//     fetch('https://api.androidhive.info/json/movies.json')
-//       .then(response => response.json())
-//       .then(json => setData(json))
-//       .catch(error => console.error(error));
-//   }, []);
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-
-//       <FlatList
-//         data={data}
-//         keyExtractor={item => item.title}
-//         renderItem={({item}) => (
-//           <RenderItem title={item.title} url={item.image} />
-//         )}
-//       />
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   logo: {
-//     width: 100,
-//     height: 100,
-//     margin: 10,
-//   },
-//   itemRow: {
-//     flexDirection: 'row',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: '400',
-//     marginHorizontal: 20,
-//   },
-// });
-
-// export default App;
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    // const isDarkMode = useColorScheme() === 'dark';
     this.state = {
       dataProvider: new DataProvider((r1, r2) => {
         return r1 !== r2;
